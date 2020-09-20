4c35b3675726773a37ee89b9ea39a5d3af8ae709
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-20 13:23:56 -0500

    Add dbcontext overrides

--- src/JRovnyBlog/ApplicationDbContext.cs
+++ src/JRovnyBlog/ApplicationDbContext.cs
@@ -1,18 +1,53 @@
 ﻿using JRovnyBlog.Api.Posts.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace JRovnyBlog
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Post> Posts { get; set; }
-        public ApplicationDbContext()
-        {
+        private readonly IConnectionService _connectionService;
 
+        public DbSet<Post> Posts { get; set; }
+
+        public ApplicationDbContext(IConnectionService connectionService)
+        {
+            _connectionService = connectionService;
         }
 
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
+            builder.UseNpgsql(_connectionService.GetDefaultConnectionString());
+        }
+
+        public override Task<int> SaveChangesAsync(
+            bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries();
+
+            foreach (var entry in entries)
+            {
+                var createdDate = entry.Metadata.FindProperty("CreatedDate");
+                var modifiedDate = entry.Metadata.FindProperty("ModifiedDate");
+                var now = DateTime.UtcNow;
+
+                if (createdDate != null)
+                {
+                    if (entry.State == EntityState.Added)
+                        entry.Property(createdDate.Name).CurrentValue = now;
+                    else if (entry.State == EntityState.Modified)
+                        entry.Property(createdDate.Name).IsModified = false;
+                }
+
+                if (modifiedDate != null)
+                    entry.Property(modifiedDate.Name).CurrentValue = now;
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+
         }
     }
 }
