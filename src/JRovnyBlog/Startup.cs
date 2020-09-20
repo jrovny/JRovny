1cc134d787d441452457c55862e89c3727bf7552
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-19 23:11:50 -0500

    Switch to latest Angular app

--- src/JRovnyBlog/Startup.cs
+++ src/JRovnyBlog/Startup.cs
@@ -18,7 +18,6 @@ namespace JRovnyBlog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
@@ -55,9 +54,6 @@ namespace JRovnyBlog
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
