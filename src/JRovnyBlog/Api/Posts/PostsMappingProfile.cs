c8396b6e334482d9e0f93c1252c9442cd8dfd7b4
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-20 01:04:51 -0500

    Add posts mapping profile

---
+++ src/JRovnyBlog/Api/Posts/PostsMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace JRovnyBlog.Api.Posts
+{
+    public class PostsMappingProfile : Profile
+    {
+        public PostsMappingProfile()
+        {
+            CreateMap<Models.Post, Data.Models.Post>();
+            CreateMap<Data.Models.Post, Models.Post>();
+        }
+    }
+}
