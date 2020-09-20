779cc18e301f767bc6e73505a9900634a0638ecb
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-20 00:28:30 -0500

    Add posts feature area

---
+++ src/JRovnyBlog/Api/Posts/Data/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JRovnyBlog.Api.Posts.Data.Models
+{
+    public class Post
+    {
+    }
+}
---
+++ src/JRovnyBlog/Api/Posts/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JRovnyBlog.Api.Posts.Models
+{
+    public class Post
+    {
+    }
+}
