39cec55bcc672cea5c4288542d26d8f4bb4fcc13
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-20 16:45:52 -0500

    Add Angular Material package and components

--- src/JRovnyBlog/ClientApp/src/app/app.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'jrovny-blog';
+  title = "JRovny Blog";
+  blogPosts = [
+    {
+      postId: 1,
+      title: "Test",
+      content: "Test",
+    },
+    {
+      postId: 2,
+      title: "Test 2",
+      content: "Test 2",
+    },
+  ];
 }
