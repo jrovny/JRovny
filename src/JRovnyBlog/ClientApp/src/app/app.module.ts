eaa338e9c36af24a61f5fba4e7f53d620a558c06
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-20 18:36:52 -0500

    Add more material components; remove ivy to accomodate Angular Flex

--- src/JRovnyBlog/ClientApp/src/app/app.module.ts
+++ src/JRovnyBlog/ClientApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
@@ -16,9 +18,11 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatSidenavModule,
     MatToolbarModule,
   ],
   providers: [],
