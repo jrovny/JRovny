83ebc4b7d94c7d442d978336346045dc20737012
Jordan Rovny <rovnyjordan@gmail.com>
2020-09-20 00:30:26 -0500

    Add AutoMapper and EF to DI

--- src/JRovnyBlog/Startup.cs
+++ src/JRovnyBlog/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,8 @@ namespace JRovnyBlog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>();
+            services.AddAutoMapper(typeof(Startup));
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
