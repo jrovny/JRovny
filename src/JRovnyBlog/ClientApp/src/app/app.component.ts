055e1c9613ba229af8620e95126cdcf69d672d4c
Jordan Rovny <rovnyjordan@gmail.com>
2020-10-03 20:17:49 -0500

    Add logic to determined sidenav opened state

--- src/JRovnyBlog/ClientApp/src/app/app.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import { AppService } from "./services/app.service";
 import { Observable } from "rxjs";
 import { PostSummary } from "./models/post-summary";
@@ -12,9 +12,26 @@ export class AppComponent implements OnInit {
   title = "JRovny";
   blogPosts$: Observable<PostSummary[]>;
 
+  rightSidenavOpened = true;
+  leftSidenavOpened = true;
+  innerWidth: number;
+
   constructor(private appService: AppService) { }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    this.setSidenavOpenedState();
     this.blogPosts$ = this.appService.getBlogPostSummaries();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = event.target.innerWidth;
+    this.setSidenavOpenedState();
+  }
+
+  private setSidenavOpenedState() {
+    this.rightSidenavOpened = this.innerWidth >= 1024;
+    this.leftSidenavOpened = this.innerWidth >= 1300;
   }
 }
