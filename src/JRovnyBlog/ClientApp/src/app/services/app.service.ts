ac8a634ce3afaad1c008a68fbb70fbe7f468ff2d
Jordan Rovny <rovnyjordan@gmail.com>
2020-10-08 19:25:06 -0500

    Change app service to get base API url from env file

--- src/JRovnyBlog/ClientApp/src/app/services/app.service.ts
+++ src/JRovnyBlog/ClientApp/src/app/services/app.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { PostSummary } from "../models/post-summary";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: "root",
 })
 export class AppService {
-  url = "https://localhost:5001/api";
+  url = environment.baseApiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getBlogPostSummaries() {
     return this.http.get<PostSummary[]>(`${this.url}/posts`);
