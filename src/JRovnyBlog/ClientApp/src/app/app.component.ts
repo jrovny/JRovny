2f78e3a7298b4a6e02dbc7c24ca79b830c0a57b7
Jordan Rovny <rovnyjordan@gmail.com>
2020-10-09 00:17:14 -0500

    Turn title button into link to home page

--- src/JRovnyBlog/ClientApp/src/app/app.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnInit } from "@angular/core";
 import { AppService } from "./services/app.service";
 import { Observable } from "rxjs";
 import { PostSummary } from "./models/post-summary";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: "app-root",
@@ -13,12 +14,13 @@ export class AppComponent implements OnInit {
   rightSidenavOpened = true;
   leftSidenavOpened = true;
   innerWidth: number;
+  baseUrl: string;
 
   constructor() { }
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
-
+    this.baseUrl = environment.baseUrl;
   }
 
   @HostListener('window:resize', ['$event'])
