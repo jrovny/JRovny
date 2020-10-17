262432c4ef2b87bfde8082e780470b447709a6a7
Jordan Rovny <rovnyjordan@gmail.com>
2020-10-17 01:38:29 -0500

    Modify formatting of blog detail page

--- src/JRovnyBlog/ClientApp/src/app/components/post-detail/post-detail.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/components/post-detail/post-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PostDetail } from 'src/app/models/post-detail';
 import { AppService } from 'src/app/services/app.service';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-post-detail',
@@ -12,12 +14,36 @@ import { AppService } from 'src/app/services/app.service';
 export class PostDetailComponent implements OnInit {
   slug: any;
   post$: Observable<PostDetail>;
+  facebookF = faFacebookF;
+  faTwitter = faTwitter;
+  innerWidth: any;
 
   constructor(private route: ActivatedRoute, private appService: AppService) { }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = event.target.innerWidth;
+  }
+
   ngOnInit(): void {
-    this.route.params.subscribe(slug => { this.slug = slug.slug; console.log(slug) });
+    this.innerWidth = window.innerWidth;
+    this.route.params.subscribe(slug => { this.slug = slug.slug; });
     this.post$ = this.appService.getBlogPostBySlug(this.slug);
   }
 
+  xsScreen() {
+    return this.innerWidth < 768;
+  }
+
+  smScreen() {
+    return this.innerWidth >= 768;
+  }
+
+  mdScreen() {
+    return this.innerWidth >= 992;
+  }
+
+  lgScreen() {
+    return this.innerWidth >= 1200
+  }
 }
--- src/JRovnyBlog/ClientApp/src/app/models/post-detail.ts
+++ src/JRovnyBlog/ClientApp/src/app/models/post-detail.ts
@@ -1,2 +1,17 @@
+import { Tag } from './tag';
+
 export interface PostDetail {
+    postId: number,
+    title: string,
+    content: string,
+    slug: string,
+    viewCount: number,
+    commentCount: number,
+    upvoteCount: number,
+    published: boolean,
+    createdDate: Date,
+    modifiedDate: Date,
+    userId: number,
+    comments: any,
+    tags: Tag[]
 }
