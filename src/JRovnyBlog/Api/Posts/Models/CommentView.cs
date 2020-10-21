a9a5c479bb26b34e64acc02bc4d244d4fd63459b
jrovny <jrovny@JROVNYDELL7540>
2020-10-20 19:44:24 -0500

    Add parent comment ID to model

--- src/JRovnyBlog/Api/Posts/Models/CommentView.cs
+++ src/JRovnyBlog/Api/Posts/Models/CommentView.cs
@@ -6,6 +6,7 @@ namespace JRovnyBlog.Api.Posts.Models
     public class CommentView
     {
         public int CommentId { get; set; }
+        public int ParentCommentId { get; set; }
         public string Content { get; set; }
         public string UserName { get; set; }
         public DateTime CreatedDate { get; set; }
