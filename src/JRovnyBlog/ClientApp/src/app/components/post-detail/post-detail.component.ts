d65139dd31fe200e975841feb3d49a7c428b987a
jrovny <jrovny@JROVNYDELL7540>
2021-02-04 22:24:56 -0600

    Add meta tags

--- src/JRovnyBlog/ClientApp/src/app/components/post-detail/post-detail.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/components/post-detail/post-detail.component.ts
@@ -6,6 +6,8 @@ import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { select, Store } from '@ngrx/store';
 import { clearPost, loadPost } from 'src/app/store/actions/app.actions';
+import { environment } from 'src/environments/environment';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-detail',
@@ -18,10 +20,13 @@ export class PostDetailComponent implements OnInit, OnDestroy {
   facebookF = faFacebookF;
   faTwitter = faTwitter;
   innerWidth: any;
+  link: string;
 
   constructor(
     private route: ActivatedRoute,
-    private store: Store<{ appState: { selectedPost: PostDetail } }>
+    private store: Store<{ appState: { selectedPost: PostDetail } }>,
+    private titleService: Title,
+    private metaService: Meta
   ) {}
 
   @HostListener('window:resize', ['$event'])
@@ -36,7 +41,21 @@ export class PostDetailComponent implements OnInit, OnDestroy {
     this.innerWidth = window.innerWidth;
     this.route.params.subscribe((params) => {
       this.slug = params.slug;
+      this.link = `${environment.baseUrl}/api/posts/${this.slug}`;
       this.store.dispatch(loadPost({ payload: this.slug }));
+    });
+
+    this.post$.subscribe((post) => {
+      if (post) {
+        this.titleService.setTitle(post.title);
+        this.metaService.addTags([
+          { name: 'og:url', content: this.link },
+          { name: 'og:type', content: 'article' },
+          { name: 'og:title', content: post.title },
+          { name: 'og:description', content: post.title },
+          { name: 'og:image', content: post.image },
+        ]);
+      }
     });
   }
 
