c9c2a2cbcac7fe2998014e0a4a6438eaafb8fe3b
jrovny <jrovny@JROVNYDELL7540>
2021-02-04 22:22:40 -0600

    Reformat post detail model

--- src/JRovnyBlog/ClientApp/src/app/models/post-detail.ts
+++ src/JRovnyBlog/ClientApp/src/app/models/post-detail.ts
@@ -2,17 +2,18 @@ import { Comment } from './comment';
 import { Tag } from './tag';
 
 export interface PostDetail {
-    postId: number,
-    title: string,
-    content: string,
-    slug: string,
-    viewCount: number,
-    commentCount: number,
-    upvoteCount: number,
-    published: boolean,
-    createdDate: Date,
-    modifiedDate: Date,
-    userId: number,
-    comments: Comment[],
-    tags: Tag[]
+  postId: number;
+  title: string;
+  content: string;
+  slug: string;
+  viewCount: number;
+  commentCount: number;
+  upvoteCount: number;
+  published: boolean;
+  createdDate: Date;
+  modifiedDate: Date;
+  userId: number;
+  comments: Comment[];
+  tags: Tag[];
+  image: string;
 }
