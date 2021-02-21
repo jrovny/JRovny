ac266ad4810ec4a66a3eafb76f30c12d0bc8412b
jrovny <jrovny@JROVNYDELL7540>
2021-02-21 17:17:22 -0600

    Replace window listenser with BreakpointObserver

--- src/JRovnyBlog/ClientApp/src/app/app.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/app.component.ts
@@ -1,40 +1,55 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = "JRovny";
+  title = 'JRovny';
   rightSidenavOpened = true;
   leftSidenavOpened = true;
-  innerWidth: number;
+  // innerWidth: number;
+  xsScreen = false;
+  smScreen = false;
+  mdScreen = false;
+  lgScreen = false;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((result) => (this.xsScreen = result.matches));
+    this.breakpointObserver
+      .observe([Breakpoints.Small])
+      .subscribe((result) => (this.smScreen = result.matches));
+    this.breakpointObserver
+      .observe([Breakpoints.Medium])
+      .subscribe((result) => (this.mdScreen = result.matches));
+    this.breakpointObserver
+      .observe([Breakpoints.Large, Breakpoints.XLarge])
+      .subscribe((result) => (this.lgScreen = result.matches));
+  }
 
   ngOnInit() {
-    this.innerWidth = window.innerWidth;
+    // this.innerWidth = window.innerWidth;
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.innerWidth = event.target.innerWidth;
-  }
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event) {
+  //   this.innerWidth = event.target.innerWidth;
+  // }
 
-  xsScreen() {
-    return this.innerWidth < 768;
-  }
-
-  smScreen() {
-    return this.innerWidth >= 768;
-  }
-
-  mdScreen() {
-    return this.innerWidth >= 992;
-  }
-
-  lgScreen() {
-    return this.innerWidth >= 1200
-  }
+  // xsScreen() {
+  //   return this.innerWidth < 768;
+  // }
+  // smScreen() {
+  //   return this.innerWidth >= 768;
+  // }
+  // mdScreen() {
+  //   return this.innerWidth >= 992;
+  // }
+  // lgScreen() {
+  //   return this.innerWidth >= 1200;
+  // }
 }
