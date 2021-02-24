2918025de0e791b4b9dbd29914f227993b2935e9
jrovny <jrovny@JROVNYDELL7540>
2021-02-23 20:52:05 -0600

    Remove reference to window object

--- src/JRovnyBlog/ClientApp/src/app/components/post-summary/post-summary.component.ts
+++ src/JRovnyBlog/ClientApp/src/app/components/post-summary/post-summary.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit, Input, HostListener } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-import { PostSummary } from "src/app/models/post-summary";
+import { PostSummary } from 'src/app/models/post-summary';
 
 @Component({
-  selector: "app-post-summary",
-  templateUrl: "./post-summary.component.html",
-  styleUrls: ["./post-summary.component.scss"],
+  selector: 'app-post-summary',
+  templateUrl: './post-summary.component.html',
+  styleUrls: ['./post-summary.component.scss'],
 })
 export class PostSummaryComponent implements OnInit {
   @Input() post: PostSummary;
-  innerWidth: number;
+  xsScreen: boolean;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((result) => (this.xsScreen = result.matches));
   }
 
-  ngOnInit(): void {
-    this.innerWidth = window.innerWidth;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.innerWidth = event.target.innerWidth;
-  }
+  ngOnInit(): void {}
 
   openBlogPost() {
     // [routerLink]="['/posts/', post.slug]"
